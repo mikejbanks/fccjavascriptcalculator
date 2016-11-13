4715cc1ae2b4e45702c862059ee76c25be7d3399
mikejbanks <mikejbanks@gmail.com>
2016-11-13 12:34:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+var allInputs=[""];
+
+var displayString;
+
+var operators=["+","-","/","*","%"];
+
+var decimalPoint=["."];
+
+var numInput=[0,1,2,3,4,5,6,7,8,9];
+
+
+function getValue(input){
+	if(decimalPoint.includes(allInputs[allInputs.length-1])===true && input==="."){
+		console.log("Two decimal points not allowed input");
+	}
+	else if(allInputs.length===1 && operators.includes(input)===false){
+		allInputs.push(input);
+	}
+else if(operators.includes(allInputs[allInputs.length-1])===false){
+	allInputs.push(input);
+}
+else if(numInput.includes(Number(input))){
+	allInputs.push(input);
+}
+update();
+}
+
+function update(){
+	displayString=allInputs.join("");
+	$("#display").val(displayString);
+}
+
+function getTotal(){
+	displayString=allInputs.join("");
+	$("#display").val(eval(displayString));
+}
+
+$(":button").on("click", function(){
+	
+	if(this.value==="AC"){
+		allInputs=[""];
+		update();
+	}
+else if(this.value==="CE"){
+	allInputs.pop();
+	update();
+}
+else if(this.value==="="){
+	getTotal();
+}
+else{
+if(allInputs[allInputs.length-1].indexOf("+","-","/","*","%")===-1){
+	getValue(this.value);
+}
+else{
+	getValue(this.value);
+}
+}
+});
+
+});
